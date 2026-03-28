2b5d1294b5ac65b980680f0772583c0b3e0bd49b
Pratik <dhanepratik543@gmail.com>
2026-03-28 10:51:01 +0530

    Interface usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.css"
 
 function App() {
-  const [input, setInput] = useState<string>("");
-  const [data, setData] = useState<string>("");
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
-    setInput(e.target.value);
+  interface Fru {
+    name: string;
+    nutrient: number;
   }
+  const fruits: Fru[] = [
+    { name: "Apple", nutrient: 50 },
+    { name: "Banana", nutrient: 60 },
+    { name: "Citrus", nutrient: 70 },
+  ];
 
-  function handleSubmit(e: React.FormEvent): void {
-    e.preventDefault();
-    setData(input);
+  const [count, setCount] = useState<number>(0);
+  const [fruit, setFruit] = useState<Fru>();
+
+  function handleClick(): void {
+    setCount(count + 1);
+    if (count % 3 === 0) {
+      setFruit(fruits[0]);
+    } else if (count % 3 === 1) {
+      setFruit(fruits[1]);
+    } else {
+      setFruit(fruits[2]);
+    }
   }
-
   return (
-    <div className="container">
-      <h1 className="hello">Hello</h1>
-
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="text">Enter Text: </label>
-
-        <input type="text" id="text" value={input} onChange={handleChange} />
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <div>
-        <h3>Output:</h3>
-        <p>{data}</p>
-      </div>
+    <div>
+      <h1>Fruit Counter</h1>
+      <h3>{count}</h3>
+      <button onClick={handleClick}>Fruit name is :{fruit?.name} Fruit nutrient is: {fruit?.nutrient}</button>
     </div>
   );
 }
