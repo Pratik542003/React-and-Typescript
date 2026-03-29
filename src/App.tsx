83fa57d9e3a33e96e16b11d597fc93ecfe20736e
Pratik <dhanepratik543@gmail.com>
2026-03-29 23:08:56 +0530

    Class and interface

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,12 @@ function App() {
 
   // ✅ state to control displayed list
   const [list, setList] = useState<Juice[]>(juices);
+  const [selected, setSelected] = useState<Juice | undefined>();
+
+  function findFruit(): void {
+    const result = juices.find((j) => j.fruit.name === "Banana");
+    setSelected(result);
+  }
 
   function showColor(name: string): string {
     const colorMap: Record<string, string> = {
@@ -43,8 +49,13 @@ function App() {
 
   // ✅ filter function
   function showNutrients(): void {
-    const filtered = juices.filter((j) => j.fruit.nutrient > 99);
+    const filtered = juices.filter((j) => j.fruit.nutrient > 49);
     setList(filtered);
+  }
+
+  function handleDelete(name: string): void {
+    const updated = list.filter((j) => j.fruit.name !== name);
+    setList(updated);
   }
 
   return (
@@ -60,9 +71,28 @@ function App() {
         ))}
       </ul>
 
-      <button onClick={showNutrients}>
-        Show Nutrient &gt; 99
-      </button>
+      <button onClick={showNutrients}>Show Nutrient &gt; 49</button>
+      <br></br>
+      <button onClick={findFruit}>Find Banana</button>
+      {selected && (
+        <div>
+          <h2 style={{ color: "red" }}>Fruit found</h2>
+          <p>{selected.fruit.name}</p>
+          <p>{selected.fruit.nutrient}</p>
+          <p>{selected.isAvailable ? "Available" : "Not Available"}</p>
+        </div>
+      )}
+
+      {list.map((j, index) => (
+  <li key={index}>
+    {j.fruit.name} - {j.fruit.nutrient}
+
+    <button onClick={() => handleDelete(j.fruit.name)}>
+      ❌
+    </button>
+  </li>
+))}
+
     </div>
   );
 }
